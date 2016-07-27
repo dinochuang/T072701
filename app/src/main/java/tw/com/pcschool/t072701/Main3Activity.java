7786e0f6ddc38b620e1a8cd7f71685861906ca2b
shaojung <shaojung@gmail.com>
2016-07-27 14:11:57 +0800

    自訂 Layout 的 ListView 2

--- app/src/main/java/tw/com/pcschool/t072701/Main3Activity.java
+++ app/src/main/java/tw/com/pcschool/t072701/Main3Activity.java
@@ -2,12 +2,19 @@ package tw.com.pcschool.t072701;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Main3Activity extends AppCompatActivity {
-
+    ListView lv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        lv3 = (ListView) findViewById(R.id.listView3);
+        MyAdapter adapter = new MyAdapter(Main3Activity.this);
+        lv3.setAdapter(adapter);
+
     }
 }
+
---
+++ app/src/main/java/tw/com/pcschool/t072701/MyAdapter.java
@@ -0,0 +1,41 @@
+package tw.com.pcschool.t072701;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Student on 2016/7/27.
+ */
+public class MyAdapter extends BaseAdapter
+{
+    Context context;
+    public MyAdapter(Context c)
+    {
+        context = c;
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView tv = new TextView(context);
+        tv.setText("Hello:" + position);
+        return tv;
+    }
+}
