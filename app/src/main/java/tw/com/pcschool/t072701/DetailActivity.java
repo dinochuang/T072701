431df4422b78d93224676931022558c45c00b726
shaojung <shaojung@gmail.com>
2016-08-01 10:18:45 +0800

    自訂 Layout 的 ListView 加入學生陣列

---
+++ app/src/main/java/tw/com/pcschool/t072701/DetailActivity.java
@@ -0,0 +1,29 @@
+package tw.com.pcschool.t072701;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent it = getIntent();
+        Bundle b = it.getExtras();
+
+        TextView tv3 = (TextView) findViewById(R.id.textView3);
+        TextView tv4 = (TextView) findViewById(R.id.textView4);
+
+        tv3.setText(b.getString("name"));
+        tv4.setText(b.getString("phone"));
+
+        ImageView iv = (ImageView) findViewById(R.id.imageView2);
+        iv.setImageResource(b.getInt("photo"));
+
+    }
+}
--- app/src/main/java/tw/com/pcschool/t072701/Main3Activity.java
+++ app/src/main/java/tw/com/pcschool/t072701/Main3Activity.java
@@ -1,7 +1,10 @@
 package tw.com.pcschool.t072701;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -22,6 +25,21 @@ public class Main3Activity extends AppCompatActivity {
         lv3 = (ListView) findViewById(R.id.listView3);
         MyAdapter adapter = new MyAdapter(Main3Activity.this, mylist);
         lv3.setAdapter(adapter);
+
+        lv3.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Bundle b = new Bundle();
+                b.putString("name", mylist.get(position).name);
+                b.putString("phone", mylist.get(position).phone);
+                b.putInt("photo", mylist.get(position).photoid);
+
+                Intent it = new Intent(Main3Activity.this, DetailActivity.class);
+                it.putExtras(b);
+                startActivity(it);
+            }
+        });
 
     }
 }
