81082061a94a043460790f5ad8c6873aa89f487d
shaojung <shaojung@gmail.com>
2016-08-01 10:48:05 +0800

    自訂 ListView 用按鈕 Click 連往下一頁

--- app/src/main/java/tw/com/pcschool/t072701/MyAdapter.java
+++ app/src/main/java/tw/com/pcschool/t072701/MyAdapter.java
@@ -1,12 +1,16 @@
 package tw.com.pcschool.t072701;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -40,14 +44,32 @@ public class MyAdapter extends BaseAdapter
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        final int p = position;
         LayoutInflater inflater = LayoutInflater.from(context);
         View v = inflater.inflate(R.layout.listitem, null);
         ImageView img = (ImageView) v.findViewById(R.id.imageView);
         img.setImageResource(data.get(position).photoid);
-        TextView tv1 = (TextView) v.findViewById(R.id.textView);
+        final TextView tv1 = (TextView) v.findViewById(R.id.textView);
         tv1.setText(data.get(position).name);
         TextView tv2 = (TextView) v.findViewById(R.id.textView2);
         tv2.setText(data.get(position).phone);
+
+        Button btn = (Button) v.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(context, tv1.getText(),Toast.LENGTH_SHORT).show();
+                Intent it = new Intent(context, DetailActivity.class);
+                Bundle b = new Bundle();
+                b.putString("name", data.get(p).name);
+                b.putString("phone", data.get(p).phone);
+                b.putInt("photo", data.get(p).photoid);
+
+                it.putExtras(b);
+                context.startActivity(it);
+            }
+        });
+
         return v;
     }
 }
